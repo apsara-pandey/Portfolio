29966b9ece9fc3b087060e08021fe91f4b35c0cd
Apsara Pandey <apsarapandey43@gmail.com>
2026-08-23 22:43:23 +0545

    Add files via upload

---
+++ Portfolio/script.js
@@ -0,0 +1,7 @@
+// Select the welcome button
+const button = document.querySelector(".welcome-btn");
+
+// When the button is clicked
+button.addEventListener("click", function() {
+    alert("Welcome to Apsara's Portfolio!");
+});
