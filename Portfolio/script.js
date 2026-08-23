67e803983b0246059f3afc8fe2276a0f2d00e810
Apsara Pandey <apsarapandey43@gmail.com>
2026-08-23 22:42:42 +0545

    Delete Portfolio/script.js

--- Portfolio/script.js
+++
@@ -1,7 +0,0 @@
-// Select the welcome button
-const button = document.querySelector(".welcome-btn");
-
-// When the button is clicked
-button.addEventListener("click", function() {
-    alert("Welcome to Apsara's Portfolio!");
-});
